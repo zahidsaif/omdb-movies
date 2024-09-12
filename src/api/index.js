de1a574bdafefb90f75bddfca3e1ebeda2ed0c18
Zahid Saif <zahidsaif1@outlook.com>
2024-09-13 02:16:37 +0530

    env fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.OMDB_API_KEY
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY
 const commonURL = `https://www.omdbapi.com/?apikey=${API_KEY}`
 
 export const OMDB_DETAILS_API = (movieID) => `${commonURL}&i=${movieID}`
